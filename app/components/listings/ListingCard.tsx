d811bdbb2ace15e6ed33c924bfaab91b32d0fa63
NovyAPP <micnowotnik@gmail.com>
2023-04-16 12:16:14 +0200

    property listing page

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,13 +6,13 @@ import Image from "next/image";
 import { format } from "date-fns";
 import React, { useCallback, useMemo } from "react";
 
-import { SafeUser } from "@/app/types";
+import { SafeUser, SafeListing } from "@/app/types";
 import useCountries from "@/app/hooks/useCountries";
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
